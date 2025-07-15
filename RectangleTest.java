a36e93701246c9078ec681e1930da727ce860c94
Nathan-cyber2004 <nathanpereira204@gmail.com>
2025-07-15 16:04:20 -0400

    Updated the rectangle code
    I updated and tested my RectangleTest program and now takes width and height from the user

--- RectangleTest.java
+++ RectangleTest.java
@@ -1,9 +1,20 @@
 import java.awt.Rectangle;
+import java.util.Scanner;
 
 public class RectangleTest {
     public static void main(String[] args) {
+        // Original Code
         Rectangle box1 = new Rectangle(10, 10, 40, 30);
         System.out.println("box1: " + box1);
+
+        // New code
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter width: "); 
+        int width = input.nextInt();
+        System.out.print("Enter height: "); 
+        int height = input.nextInt();
+
+
     }
 
 }
